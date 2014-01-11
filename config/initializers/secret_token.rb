0abc88772ded574d7c90f0ee833d510ed12d6512
Ben Calegari <bencalegari@gmail.com>
2014-01-11 14:19:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moody::Application.config.secret_key_base = '39cebc55cc275911f0520533007eb2fdfb70100fe4e4789daa153370f1465e4f62a55e60b4d053676e9a47c6d95f8e5234d9ed1055ec7d193b1086e9bd81dcc5'
